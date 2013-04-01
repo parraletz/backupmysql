8fd74c2176dc3029de9e129c72d7189e241e71cf
Alex Parra <alex@parralito.tralix.com>
2013-04-01 14:07:48 -0600

    Se quita la notificacion por email

--- trmysqlbp.py
+++ trmysqlbp.py
@@ -2,8 +2,9 @@ import argparse
 import os
 import subprocess
 from datetime import date
+import time
 import gzip
-
+import logging
 
 parser = argparse.ArgumentParser(description="Realiza respaldo completo de la base de datos MySQL", version="trBackup 1.0")
 parser.add_argument( '-d','--database', dest='database', help='Nombre de la base de datos' )
@@ -21,36 +22,19 @@ fecha = date.today()
 hoy= fecha.strftime("%d%m%Y")
 filename = database + '_' + hoy + '_BACKUP.sql'
 
-class Backup():
-
-	def MakeDump():
-		
-		outputfile = open('/backups/db/'+filename, 'w')
-		subprocess.Popen(['mysqldump', '-u'+username, '-p'+password, '--single-transaction', '--opt', database], stdout=outputfile)
-		outputfile.close()
+def MakeDump():
 
 
-	def makeGzip():
-		f_in = open('/backups/db/'+filename, 'rb')
-		f_out = gzip.open('/backups/db/'+filename+'.gz','wb')
-		f_out.writelines(f_in)
-		f_out.close()
-		f_in.close()
+	outputfile = open('/backups/db/'+filename, 'w') # Crea el archivo donde se volcara la base de datos 
+	subprocess.Popen(['/usr/local/mysql/bin/mysqldump', '-u'+username, '-p'+password, '--single-transaction', database], stdout=outputfile) # Se crear el dump
+	outputfile.close()
 
+	time.sleep(15)
 
-	def sendMail():
-	    sendmail_location = "/usr/sbin/sendmail" 
-	    p = os.popen("%s -t" % sendmail_location, "w")
-	    p.write("From: %s\n" % "parra@tralix.com")
-	    p.write("To: %s\n" % "soporte@tralix.com")
-	    p.write("Subject: Se ha creado el respaldo de la base de datos de la TEA " + tea + " del dia " + hoy + "\n")
-	    p.write("\n") 
-	    p.write("Se ha creado correctamente el respaldo de la base de datos " + database + "correspondiente al dia " + hoy + "el archivo se encuentra en la ruta " + filename )
-	    status = p.close()
-	    if status != 0:
-	           print "Sendmail exit status", status
+	f_in = open('/backups/db/'+filename, 'rb')# Se crea el archivo comprimido
+	f_out = gzip.open('/backups/db/'+filename+'.gz','wb')
+	f_out.writelines(f_in)
+	f_out.close()
+	f_in.close()
 
-
-	MakeDump()
-	makeGzip()
-	sendMail()
+MakeDump()
