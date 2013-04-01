0e5492baea15a2a2f2ad0b79e4c20fbbf37eb6a6
Alex Parra <parraletz@gmail.com>
2013-03-31 21:16:07 -0600

    Add subprocess

--- trmysqlbp.py
+++ trmysqlbp.py
@@ -1,11 +1,13 @@
 import argparse
 import os
 import subprocess
+from datetime import date
 
-parser = argparse.ArgumentParser(description="Realiza respaldo completo de la base de datos TEA", version="trBackup 1.0")
+parser = argparse.ArgumentParser(description="Realiza respaldo completo de la base de datos MySQL", version="trBackup 1.0")
 parser.add_argument( '-d','--database', dest='database', help='Nombre de la base de datos' )
 parser.add_argument( '-u','--username', dest='username', help='Usuario de base de datos')
 parser.add_argument( '-p','--password', dest='password',  help='Password')
+parser.add_argument( '-m', '--host', dest='host', help='Hostname')
 args = parser.parse_args()
 
 
@@ -14,12 +16,8 @@ username = args.username
 password = args.password
 
 
-
-
-def MakeDump():
-	def __init__():
-		pass
-	print username + " " + database + " " + password
-
-
-
+outputfile = open('/backups/'+database+'-BACKUP.sql', 'w')
+ 
+subprocess.Popen(['mysqldump', '-uUSERNAME', '-pPASSWORD', '--single-transaction', '--opt', db], stdout=outputfile)
+ 
+outputfile.close()
